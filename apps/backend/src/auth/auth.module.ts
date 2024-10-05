f45a49c9e472c73dee7225651fed78f845ad7916
Christian C. Del Moro <christian.delmoro@protonmail.com>
2024-10-05 18:03:02 -0300

    Bla

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,3 +1,31 @@
+// import { Module } from "@nestjs/common";
+// import { AuthController } from "./auth.controller";
+// import { AuthService } from "./auth.service";
+// import { JwtModule } from "@nestjs/jwt";
+// import { PassportModule } from "@nestjs/passport";
+// import { LocalStrategy } from "./strategies/local.strategy";
+// import { JwtStrategy } from "./strategies/jwt.strategy";
+// import { ConfigModule, ConfigService } from "@nestjs/config";
+
+// @Module({
+//   imports: [
+//     PassportModule,
+//     JwtModule.registerAsync({
+//       imports: [ConfigModule],
+//       inject: [ConfigService],
+//       useFactory: (configService: ConfigService) => ({
+//         secret: configService.get<string>("JWT_SECRET"),
+//         signOptions: {
+//           expiresIn: "5m",
+//         },
+//       }),
+//     }),
+//   ],
+//   controllers: [AuthController],
+//   providers: [AuthService, LocalStrategy, JwtStrategy],
+// })
+// export class AuthModule {}
+
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
@@ -5,20 +33,13 @@ import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { LocalStrategy } from "./strategies/local.strategy";
 import { JwtStrategy } from "./strategies/jwt.strategy";
-import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
     PassportModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>("JWT_SECRET"),
-        signOptions: {
-          expiresIn: "5m",
-        },
-      }),
+    JwtModule.register({
+      secret: "abc123",
+      signOptions: { expiresIn: "1h" },
     }),
   ],
   controllers: [AuthController],
--- apps/backend/src/auth/strategies/jwt.strategy.ts
+++ apps/backend/src/auth/strategies/jwt.strategy.ts
@@ -1,15 +1,34 @@
+// import { Injectable } from "@nestjs/common";
+// import { ConfigService } from "@nestjs/config";
+// import { PassportStrategy } from "@nestjs/passport";
+// import { Strategy, ExtractJwt } from "passport-jwt";
+
+// @Injectable()
+// export class JwtStrategy extends PassportStrategy(Strategy) {
+//   constructor(private configService: ConfigService) {
+//     super({
+//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//       ignoreExpiration: false,
+//       secretOrKey: configService.get<string>("JWT_SECRET"),
+//     });
+//   }
+
+//   validate(payload: any) {
+//     return payload;
+//   }
+// }
+
 import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy, ExtractJwt } from "passport-jwt";
+import { ExtractJwt, Strategy } from "passport-jwt";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private configService: ConfigService) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>("JWT_SECRET"),
+      secretOrKey: "abc123",
     });
   }
 
